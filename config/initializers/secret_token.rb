c98f2818cdd36c1ffa4b8fde6f396344d3fb68ec
unknown <Administrator@celab1-9.comp1-domain.spit.ac.in>
2012-09-16 15:21:42 +0530

    all update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StockApp1::Application.config.secret_token = 'bd953ee61414592ea2349343401c2298180a670de5a6f7171b956b6299a3b2f896c94193507f90133141981d54a75b34502e1e6f709a7dc4cda47d0e69e0e047'
